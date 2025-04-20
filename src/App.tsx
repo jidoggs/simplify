1e35335110b2f5207d528ae7b96488c530ff712a
Olajide Shonuga <65728252+jidoggs@users.noreply.github.com>
2025-04-20 23:46:38 +0100

    added vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { generateRoute } from "./common/routes/generate-route";
 import { routes } from "./common/routes/routes";
+import { routeLinks } from "./common/routes/route-links";
 
 function App() {
   return (
     <Routes>
       <Route>{routes.map((route) => generateRoute(route))}</Route>
+      <Route
+        path="/"
+        element={<Navigate to={routeLinks.auth.login} replace />}
+      />
       <Route path="*" element={<p>NOT FOUND</p>} />
     </Routes>
   );
